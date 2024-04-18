e961f48dd92e731cb5b24d6f1decdaeac4ff5964
SamarSiddiqui <samarsiddiqui3010@gmail.com>
2024-04-18 23:06:41 +0530

    Added The Functionality Of Color Change to Body,Quote,Author & Button

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
-   let quoteHeading = document.getElementById('quote')
-    let authorHeading = document.getElementById('author')
-    let button = document.querySelector('button')
-    
-    
-    async function fetchData () {
-        let response = await fetch("./data.json")
-        let data = await response.json()
-        
-       
-        return data
-    }
-    
-    let myProm = fetchData()
-  
-    
-    let randomQuotes = Math. floor(Math.random()*103)
-   
-    
-    
-    
-    function quotesGenerator (sher,shayar) {
-        // let [firstSentence, restOfQuote] = sher.split('@@');
-        // let formattedQuote = `${firstSentence}<br>${restOfQuote}`;
-        quoteHeading.textContent = sher
-        authorHeading.textContent = shayar
-    
-    
-    }
+const quoteHeading = document.getElementById('quote');
+const authorHeading = document.getElementById('author');
+const button = document.querySelector('button');
+const body = document.querySelector('body');
 
-    button.addEventListener('click', () => {
-        myProm.then((data) => {
-            // console.log(data.shayri.length);
-            let randomQuotes = Math.floor(Math.random() * 0);
-            console.log(randomQuotes);
-            let sher = data.shayri[randomQuotes].sher;
-            console.log(sher);
-            let shayar = data.shayri[randomQuotes].shayar;
-            quotesGenerator(sher, shayar);
-        }).catch(() => console.log('Promise Not Resolved'));
-    });
+randomColor();
 
+async function fetchData() {
+    try {
+        let response = await fetch("./data.json");
+        let data = await response.json();
+        return data;
+    } catch (error) {
+        console.error("Error fetching data", error);
+    }
+}
+
+let myProm = fetchData();
+
+myProm.then((data) => {
+    var randomQuotes = Math.floor(Math.random() * data.shayri.length);
+    quoteHeading.innerHTML = data.shayri[randomQuotes].sher;
+    authorHeading.textContent = data.shayri[randomQuotes].shayar;
+});
+
+function randomColor() {
+    const randomColorGen = `rgb(${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)}, ${Math.floor(Math.random() * 255)})`;
+    body.style.backgroundColor = randomColorGen;
+    button.style.backgroundColor = randomColorGen;
+    quoteHeading.style.color = randomColorGen;
+    authorHeading.style.color = randomColorGen;
+}
+
+function quotesGenerator(sher, shayar) {
+    quoteHeading.innerHTML = sher;
+    authorHeading.textContent = shayar;
+}
+
+button.addEventListener('click', () => {
+    myProm.then((data) => {
+        var randomQuotes = Math.floor(Math.random() * data.shayri.length);
+        const { sher, shayar } = data.shayri[randomQuotes];
+        randomColor();
+        quotesGenerator(sher, shayar);
+    }).catch(() => console.log('Promise Not Resolved'));
+});
